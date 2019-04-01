4973f4f81e6d4cf64cd7ae9e473a1adc18c9c589
fuzhaoliang <fuzhaoliang@111.com.cn>
2019-04-01 15:59:49 +0800

    调整select

--- mysql/SqlAlchemy/example_1_单表过滤查询.py
+++ mysql/SqlAlchemy/example_1_单表过滤查询.py
@@ -1,46 +1,8 @@
-from sqlalchemy import Column, String, create_engine, BIGINT, Integer, Date, DateTime
+from mysql.SqlAlchemy.Endurance import Statistics
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+
 from JsonEncode import to_json_str
 from mysql.MyLog import logger
-import datetime
-
-# 创建对象的基类
-Base = declarative_base()
-
-
-# 定义持久化类
-# 继承declarative_base()基类，SqlAlchemy将默认实现__init__函数,
-# 并且将所有映射的字段放入形参中并进行赋值
-class Statistics(Base):
-    # 表名 必须定义
-    __tablename__ = 'statistics'
-
-    # 主键
-    id = Column(Integer, primary_key=True)
-
-    im_uid = Column(name="im_uid", type_=String(100))
-
-    effective_advisory = Column(name="effective_advisory", type_=Integer)
-
-    first_avg_reply = Column("first_avg_reply", Integer)
-
-    avg_reply = Column(name="avg_reply", type_=Integer)
-
-    first_reply_session_id = Column(name="first_reply_session_id", type_=String(2000))
-
-    operating_data_time = Column("operating_data_time", Date)
-
-    create_user = Column("create_user", String(50))
-
-    create_time = Column("create_time", DateTime, default=datetime.datetime.now)
-
-    update_user = Column(name="update_user", type_=String(50))
-
-    update_time = Column(name="update_time", type_=DateTime)
-
-    def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 if __name__ == "__main__":
